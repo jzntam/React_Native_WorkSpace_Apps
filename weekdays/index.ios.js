4fb7c351128da5cfc19a4e079127771de3a77d57
Jason Tam <jzntam@gmail.com>
2016-10-02 10:08:15 -0700

    Code Basic Single Component App from Scratch (Weekdays App)

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -1,53 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// Import Libraries
+var React       = require('react-native');
+var AppRegistry = React.AppRegistry;
+var View        = React.View;
+var Text        = React.Text;
+var StyleSheet  = React.StyleSheet;
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class weekdays extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+var Weekdays = React.createClass({
+  render: function() {
+    return <View style={ styles.container }>
+      <Text>
+        Days of the week:
+      </Text>
+    </View>
   }
-}
+});
 
-const styles = StyleSheet.create({
+// Styling the React Component
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    alignItems: 'center'
+  }
 });
 
-AppRegistry.registerComponent('weekdays', () => weekdays);
+// Tells React Native, to use the Component Weekdays for the app named 'weekdays'
+AppRegistry.registerComponent('weekdays', function() {
+  return Weekdays
+});
