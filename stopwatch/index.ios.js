7c331ee94f1d99294a853a4088d8088fd874c955
Jason Tam <jzntam@gmail.com>
2016-10-02 17:56:57 -0700

    Add time elapsed to StopWatch

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -1,18 +1,26 @@
+var formatTime = require('minutes-seconds-milliseconds')
+
 var React = require('react-native')
 var {
   Text,
   View,
+  TouchableHighlight,
   AppRegistry,
-  StyleSheet
+  StyleSheet,
 } = React
 
 var StopWatch = React.createClass({
+  getInitialState: function() {
+    return {
+      timeElapsed: null
+    }
+  },
   render: function() {
     return <View style={ styles.container }>
       <View style={ [styles.header, this.border('yellow')] }>
         <View style={ [styles.timerWrapper, this.border('red')] }>
           <Text>
-            00:00:00
+            {formatTime( this.state.timeElapsed )}
           </Text>
         </View>
         <View style={ [styles.buttonWrapper, this.border('green')] } >
@@ -28,18 +36,29 @@ var StopWatch = React.createClass({
     </View>
   },
   startStopButton: function() {
-  return <View>
-    <Text>
-      Start
-    </Text>
-  </View>
+    return <TouchableHighlight
+      underlayColor='grey'
+      onPress={ this.handleStartPress }>
+      <Text>
+        Start
+      </Text>
+    </TouchableHighlight>
   },
   lapButton: function() {
-  return <View>
-    <Text>
-      Lap
-    </Text>
-  </View>
+    return <View>
+      <Text>
+        Lap
+      </Text>
+    </View>
+  },
+  handleStartPress: function() {
+    var startTime = new Date();
+
+    setInterval(() => {
+      this.setState({
+        timeElapsed: new Date() - startTime
+      });
+    }, 30);
   },
   border: function(color) {
     return {
